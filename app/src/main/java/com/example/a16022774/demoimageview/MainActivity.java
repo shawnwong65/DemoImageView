3be3457d44348cb0b7d6b8e4c4ed35309e627249
shawnwong65 <16022774@rp.edu.sg>
2018-04-23 11:50:45 +0800

    Updated

--- app/src/main/java/com/example/a16022774/demoimageview/MainActivity.java
+++ app/src/main/java/com/example/a16022774/demoimageview/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.a16022774.demoimageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivDay2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivDay2 = (ImageView) findViewById(R.id.imageView2);
+        ivDay2.setImageResource(R.drawable.day2);
+
     }
 }
